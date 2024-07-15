8fab163a7ebf5bfb4c3db3af1f5aee9a3b2bffc0
Nethuja Gunawardane <nethujasand@gmail.com>
2024-07-15 23:24:06 +0530

    Update math.bat

--- math.bat
+++ math.bat
@@ -1,7 +1,10 @@
 @echo off
 
 set math=%1
+::Set variable to first parameter passed to the script
 
 echo print(%math%) > %temp%/mathbatch.py
+::Make python file
 
 python %temp%/mathbatch.py
+::Display output
