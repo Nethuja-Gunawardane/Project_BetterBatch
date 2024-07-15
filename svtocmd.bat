f15137c352b53c8e166006d2df78210ede4b57c1
Nethuja Gunawardane <nethujasand@gmail.com>
2024-07-15 23:27:06 +0530

    Update svtocmd.bat

--- svtocmd.bat
+++ svtocmd.bat
@@ -2,5 +2,7 @@
 
 	set varname=%1
 	set command=%2
+	::Set variables to first and second parameters passed to the script
 
 for /f "tokens=*" %%i in ('%command%') do set %varname%=%%i
+::Set the variable to the output of the command using for /f
