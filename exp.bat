e512e9f43b32cc6b9a892f1e04c1735d328ab873
Nethuja Gunawardane <nethujasand@gmail.com>
2024-08-16 18:22:46 +0530

    Add comments

--- exp.bat
+++ exp.bat
@@ -2,9 +2,10 @@
 
     set "base=%1"
     set "power=%2"
+    ::Set variables to parameters passed on to the script
 
-    echo print (%base%**%power%) > %temp%\exp.py
+    echo print^(%base%**%power%^) > %temp%\exp.py
+    ::Make Python script in temp folder
     
     python %temp%\exp.py
-
-
+    ::Execute Python script
